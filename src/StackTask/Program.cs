8fe1a55e488f27dfda31d6e16b5d1a38262860a0
Momen Bazzar <momen.b2001@gmail.com>
2022-02-18 16:19:49 +0200

    adding Push, Pop, Peek, Any methods

--- src/StackTask/Program.cs
+++ src/StackTask/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 
-namespace StackTask // Note: actual namespace depends on the project name.
+namespace StackTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new STACK<int>();
+            stack.Push(12);
+            stack.Push(10);
+            stack.Push(35);
+
+            while (stack.Any())
+            {
+                Console.WriteLine(stack.Pop().data);
+            }
         }
+    }
+}
+
+public class STACK<T> 
+{
+    public class Node<T>
+    {
+        public Node<T> next = null;
+        public T data;
+    }
+
+    private Node<T> root = null;
+
+    public bool Any()
+    {
+        return root != null;
+    }
+
+    public Node<T> Peek
+    {
+        get
+        {
+            return root;
+        }
+    }
+
+    public void Push(T value)
+    {
+        var node = new Node<T>();
+        node.data = value;
+        node.next = this.root;
+        this.root = node;
+    }
+
+    public Node<T> Pop()
+    {
+        var peek = this.root;
+
+        if (this.root != null) this.root = this.root.next;
+
+        return peek;
     }
 }
