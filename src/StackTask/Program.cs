c6bf54df613c3222c4357aca0f3149f86411699e
Momen Bazzar <momen.b2001@gmail.com>
2022-02-19 17:09:12 +0200

    fix naming for some variables, separating files

--- src/StackTask/Program.cs
+++ src/StackTask/Program.cs
@@ -6,7 +6,7 @@ namespace StackTask
     {
         static void Main(string[] args)
         {
-            var stack = new STACK<char>();
+            var stack = new Stack<char>();
             stack.Push('r');
             stack.Push('b');
             stack.Push('q');
@@ -15,64 +15,5 @@ namespace StackTask
             // stack.Clear();
             stack.Print();
         }
-    }
-}
-
-public class STACK<T> 
-{
-    public class Node<T>
-    {
-        public Node<T> next = null;
-        public T data;
-    }
-
-    private Node<T> root = null;
-
-    public bool Any()
-    {
-        return root != null;
-    }
-
-    public Node<T> Peek
-    {
-        get
-        {
-            return root;
-        }
-    }
-
-    public void Push(T value)
-    {
-        var node = new Node<T>();
-        node.data = value;
-        node.next = this.root;
-        this.root = node;
-    }
-
-    public Node<T> Pop()
-    {
-        var peek = this.root;
-
-        if (this.root != null) this.root = this.root.next;
-
-        return peek;
-    }
-
-    public void Clear()
-    {
-        this.root = null;
-    }
-
-    public void Print()
-    {
-        if (!this.Any()) return;
-
-        var node = this.root;
-        while (node != null)
-        {
-            Console.Write(node.data + " ");
-            node = node.next;
-        }
-        
     }
 }
